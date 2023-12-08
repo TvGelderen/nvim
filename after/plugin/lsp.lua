26da6ecb3271639cd5911108fd6ebfc9e2c2d974
TvGelderen <tvgelderen@outlook.com>
2023-12-08 12:58:30 +0100

    initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = { 'tsserver', 'eslint', 'volar', 'cssls' },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
---
+++ init.lua
@@ -0,0 +1,5 @@
+if vim.g.vscode then
+    print("VSCode")
+else
+    require("custom")
+end
