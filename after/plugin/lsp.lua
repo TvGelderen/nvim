a28c80fcd1194daffe66da536b2faa51bea5493b
TvGelderen <tvgelderen@outlook.com>
2023-12-25 21:18:09 +0100

    updated lsp to include golang

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,7 +6,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver', 'eslint', 'volar', 'cssls' },
+  ensure_installed = { 'tsserver', 'eslint', 'volar', 'cssls', 'rust_analyzer', 'gopls' },
   handlers = {
     lsp_zero.default_setup,
   },
